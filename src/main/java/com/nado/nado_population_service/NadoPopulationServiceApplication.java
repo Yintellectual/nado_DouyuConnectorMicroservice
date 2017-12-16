91015a7d55715740a00cb1dcae83e0e26f7a68f6
Yu Chen <intellectual.y@gmail.com>
2017-12-16 11:50:59 +0800

    starter

---
+++ src/main/java/com/nado/nado_population_service/NadoPopulationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.nado.nado_population_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NadoPopulationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NadoPopulationServiceApplication.class, args);
+	}
+}
